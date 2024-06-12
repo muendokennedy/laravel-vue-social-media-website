9b55b1cde746b1a530d2bd044ea1fb6cac07aad3
muendokennedy <kennedymunyao999@gmail.com>
2024-06-12 15:08:35 +0300

    Cleaned unnecessary routes and vue files

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps();
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <h2>This is the landing page</h2>
+</template>
+<style>
+
+
+</style>
