0d9e5804dba9a0e17c24ae276e104fe868283b14
muendokennedy <kennedymunyao999@gmail.com>
2024-06-12 15:51:21 +0300

    Deleted the default styling of the breeze and started styling custom styling for the homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Inertia\Inertia;
 class HomeController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
         return Inertia::render('Home');
     }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,9 +7,31 @@ defineProps();
 
 <template>
     <Head title="Welcome" />
-    <h2>This is the landing page</h2>
+    <div class="grid grid-cols-12 gap-3">
+        <div class="col-span-3">
+            <h2>My Groups</h2>
+            <div>
+                You are not joined to any groups
+            </div>
+            <div>
+                <div class="flex">
+                    <img src="" alt="">
+                    <div>
+                        <h3>Laravel Developers</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-6">
+            Posts
+        </div>
+        <div class="col-span-3">
+            Followers
+        </div>
+    </div>
 </template>
-<style>
+<style scoped>
 
 
 </style>
