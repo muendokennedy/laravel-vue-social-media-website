4952b3e78cec7684903a74b3c36172ef4da38135
muendokennedy <kennedymunyao999@gmail.com>
2024-06-30 17:55:14 +0300

    Merged code for attachments preview both from the server and from the browser

--- resources/js/Components/app/PostModal.vue
+++ resources/js/Components/app/PostModal.vue
@@ -2,7 +2,7 @@
     import { computed, ref, watch } from 'vue'
     import {TransitionRoot, TransitionChild, Dialog, DialogPanel, DialogTitle} from '@headlessui/vue'
     import PostUserInfo from '@/Components/app/PostUserInfo.vue';
-    import { XMarkIcon, PaperClipIcon, BookmarkIcon } from '@heroicons/vue/24/solid'
+    import { XMarkIcon, PaperClipIcon, BookmarkIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/solid'
     import { useForm } from '@inertiajs/vue3';
     import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
     import { isImage } from '@/helpers.js'
@@ -19,7 +19,8 @@
     const form = useForm({
         id: null,
         body: '',
-        attachments: []
+        attachments: [],
+        deleted_file_ids: []
     })
 
     const attachmentFiles = ref([])
@@ -112,8 +113,17 @@
     }
 
     const removeFile = (file) => {
-        attachmentFiles.value = attachmentFiles.value.filter(f => f !== file)
+        if(file.file){
+            attachmentFiles.value = attachmentFiles.value.filter(f => f !== file)
+        } else {
+            form.deleted_file_ids.push(file.id)
+            file.deleted = true
+        }
     }
+
+    const computedAttachments = computed(() => {
+        return [...attachmentFiles.value, ...props.post.attachments]
+    })
 
 </script>
 <template>
@@ -160,20 +170,25 @@
                     <div class="p-4">
                         <PostUserInfo :post="post" :show-time="false" class="mb-4"/>
                         <ckeditor :editor="editor" v-model="form.body" :config="editorConfig"></ckeditor>
+                        <pre>{{  form.deleted_file_ids }}</pre>
                         <div class="grid gap-3 my-3"
                         :class="[
-                            attachmentFiles.length === 1 ? 'grid-cols-1' :  'grid-cols-2'
+                            computedAttachments.length === 1 ? 'grid-cols-1' :  'grid-cols-2'
                             ]">
-                            <template v-for="(myFile, index) in attachmentFiles" :key="index">
+                            <template v-for="(myFile, index) in computedAttachments" :key="index">
                                 <div class="group aspect-square bg-blue-100 flex flex-col items-center justify-center text-gray-500 relative">
+                                    <div v-if="myFile.deleted" class="z-10 absolute left-0 bottom-0 right-0 py-2 px-3 text-sm bg-black text-white flex items-center justify-between">
+                                        To be deleted
+                                        <ArrowUturnLeftIcon class="size-4 cursor-pointer"/>
+                                    </div>
                                     <button @click="removeFile(myFile)" class="absolute z-20 right-1 top-1 w-7 h-7 flex items-center justify-center bg-black/30 hover:bg-black/70 text-white rounded-full">
                                         <XMarkIcon class="size-5"/>
                                     </button>
-                                    <img v-if="isImage(myFile.file)" :src="myFile.url" alt="" class="object-cover w-full h-full">
-                                    <template v-else>
+                                    <img v-if="isImage(myFile.file ?? myFile)" :src="myFile.url" alt="" class="object-cover w-full h-full" :class="myFile.deleted ? 'opacity-50' : ''">
+                                    <div v-else class="flex justify-center items-center flex-col" :class="myFile.deleted ? 'opacity-50' : ''">
                                         <PaperClipIcon class="w-10 h-10 mb-3"/>
-                                        <small class="text-center">{{ myFile.file.name }}</small>
-                                    </template>
+                                        <small class="text-center">{{ (myFile.file ?? myFile).name }}</small>
+                                    </div>
                                 </div>
                             </template>
                         </div>
