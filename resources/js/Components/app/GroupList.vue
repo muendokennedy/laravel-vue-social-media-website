a31fedc5b3cca53e4076537a74a36eafe9a344e5
muendokennedy <kennedymunyao999@gmail.com>
2024-07-18 05:29:09 +0300

    Gave proper positioning to the new group button

--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -1,10 +1,6 @@
 <script setup>
 import GroupListItems from './GroupListItems.vue'
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
-import GroupModal from '@/Components/app/GroupModal.vue'
-import { ref } from 'vue';
-
-const showNewGroupModal = ref(false)
 
 </script>
 <template>
@@ -14,7 +10,6 @@ const showNewGroupModal = ref(false)
                 <DisclosureButton class="text-left w-full">
                     <div class="flex justify-between items-center">
                         <h2 class="text-xl font-bold">My Groups</h2>
-                        <button @click="showNewGroupModal = true" class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2 capitalize">new group</button>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 transition-all" :class="open ? 'rotate-90 transform' : ''">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                         </svg>
@@ -23,17 +18,13 @@ const showNewGroupModal = ref(false)
                 <DisclosurePanel>
                     <GroupListItems/>
                 </DisclosurePanel>
-                <div class="flex justify-end">
-                </div>
             </Disclosure>
         </div>
         <div class="h-full overflow-hidden flex-col hidden lg:flex">
             <div class="flex justify-between">
                 <h2 class="text-xl font-bold">My Groups</h2>
-                <button @click="showNewGroupModal = true" class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2 capitalize">new group</button>
             </div>
             <GroupListItems/>
         </div>
     </div>
-    <GroupModal v-model="showNewGroupModal"/>
 </template>
--- resources/js/Components/app/GroupListItems.vue
+++ resources/js/Components/app/GroupListItems.vue
@@ -2,12 +2,17 @@
 import GroupItem from './GroupItem.vue';
 import TextInput from '../TextInput.vue';
 import { ref } from 'vue';
+import GroupModal from '@/Components/app/GroupModal.vue'
 
+const showNewGroupModal = ref(false)
 const searchKeyword = ref('')
 
 </script>
 <template>
-    <TextInput v-model="searchKeyword" placeHolder="Search here..." class="w-full mt-4"/>
+    <div class="flex gap-2 mt-4">
+        <TextInput v-model="searchKeyword" placeHolder="Search here..." class="w-full"/>
+        <button @click="showNewGroupModal = true" class="text-sm bg-indigo-500 hover:bg-indigo-600 text-white rounded py-1 px-2 capitalize w-fit text-nowrap">new group</button>
+    </div>
     <div class="mt-3 h-96 lg:flex-1 overflow-auto">
         <div v-if="false" class="text-gray-400 text-center p-3">
             You are not joined to any groups
@@ -31,4 +36,5 @@ const searchKeyword = ref('')
             <GroupItem title="Vue.js Developers" image="/man1.jpg" description="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
         </div>
     </div>
+    <GroupModal v-model="showNewGroupModal"/>
 </template>
