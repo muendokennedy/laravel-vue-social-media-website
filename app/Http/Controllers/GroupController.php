7bb36e9b5f76cbf71c027d2c9b499db40db5878c
muendokennedy <kennedymunyao999@gmail.com>
2024-08-07 07:34:25 +0300

    Fixed the loadmore functionality in the group profile

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -62,7 +62,7 @@ class GroupController extends Controller
 
 
         if($request->wantsJson()){
-            return PostResource::collection($posts);
+            return $posts;
         }
 
         $users = User::query()
